fe8734725cbadaa774bd33b59dfc41eb98faad48
lfpc8376 <lfpc8376@d88da162-385a-0410-a532-eae3b6c49809>
2015-03-26 13:42:12 +0000

    [TECH] add soapui.fail.detail for tests
    git-svn-id: https://www.forge.orange-labs.fr/svnroot/softcu/soapui-spring-test/trunk@45346 d88da162-385a-0410-a532-eae3b6c49809

--- src/main/java/com/smartbear/soapui/spring/SoapUiTestCase.java
+++ src/main/java/com/smartbear/soapui/spring/SoapUiTestCase.java
@@ -48,6 +48,7 @@ public class SoapUiTestCase extends junit.framework.TestCase {
 		WsdlTestCaseRunner runner = (WsdlTestCaseRunner) testCase.run(new PropertiesMap(), false);
 		List<TestStepResult> results = runner.getResults();
 		LOGGER.debug(getMessages(testCase.getName(), results));
+		boolean isSoapUiFailDetailLog = Boolean.getBoolean("soapui.fail.detail");
 
 		Collection<TestStepResult> filteredResult = Collections2.filter(results, new Predicate<TestStepResult>() {
 			public boolean apply(TestStepResult testStepResult) {
@@ -55,8 +56,8 @@ public class SoapUiTestCase extends junit.framework.TestCase {
 			}
 		});
 
-		assertThat(filteredResult).overridingErrorMessage(
-				getMessages(testCase.getName(), (Boolean.getBoolean("soapui.fail.detail") ? results : filteredResult))).isEmpty();
+		assertThat(filteredResult).overridingErrorMessage(getMessages(testCase.getName(), isSoapUiFailDetailLog ? results : filteredResult))
+				.isEmpty();
 		assertThat(runner.getStatus()).isEqualTo(Status.FINISHED);
 	}
 
